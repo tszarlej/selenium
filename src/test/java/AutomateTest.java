ab96b8c5eb805d57030b6075e0f49a650dd692fe
Tomasz Szarlej <t.szarlej@kainos.com>
2016-04-05 13:48:42 +0200

    Switching to the ChromeDriver

--- src/test/java/AutomateTest.java
+++ src/test/java/AutomateTest.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -11,9 +13,14 @@ public class AutomateTest {
 	private WebDriver driver;
 	private static final String BASE_URL = "http://www.allegro.pl";
 
+	@BeforeClass
+	public void generalSetUp() {
+		System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver_mac");
+	}
+
 	@BeforeMethod
-	public void setUp() {
-		this.driver = new FirefoxDriver();
+	public void setUp() {;
+		this.driver = new ChromeDriver();
 		driver.get(BASE_URL);
 	}
 
